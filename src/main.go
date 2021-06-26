7bf0c8252e6a8aa6b9cf707343452435db3bfa1c
luyomo <syunka.tyo@gmail.com>
2021-06-26 23:33:27 +0800

    Generate the data type data

--- src/main.go
+++ src/main.go
@@ -3,6 +3,11 @@ package main
 import (
     "database/sql"
     "fmt"
+    "time"
+    "strings"
+    "math/rand"
+    "math"
+    "strconv"
     _ "github.com/go-sql-driver/mysql"
 )
 
@@ -18,10 +23,13 @@ type TableMeta struct {
     columnKey               sql.NullString     `json:"COLUMN_KEY"`
 }
 
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
 // Refer to: https://github.com/go-sql-driver/mysql/wiki/Examples
 //           https://medium.com/aubergine-solutions/how-i-handled-null-possible-values-from-database-rows-in-golang-521fb0ee267
 func main() {
+    rand.Seed(time.Now().UnixNano())
+
     db, err := sql.Open("mysql", "root@tcp(172.16.6.200:4000)/dev_tb6290_4")
     if err != nil {
         panic(err.Error())  // Just for example purpose. You should use proper error handling instead of panic
@@ -34,25 +42,103 @@ func main() {
     rows, err := db.Query(queryStr)
     defer rows.Close()
     fmt.Printf("Starting before fetching the data ")
+
+    var listTableMeta []TableMeta
     
     for rows.Next() {
         if err := rows.Scan(&tableMeta.columnName, &tableMeta.ordinalPosition, &tableMeta.dataType, &tableMeta.characterMaximumLength, &tableMeta.numericPrecision, &tableMeta.numericScale, &tableMeta.datetimePrecision, &tableMeta.columnType, &tableMeta.columnKey); err != nil {
             fmt.Printf("The square number of 13 is: %d", tableMeta.columnName)
         }
-        if (tableMeta.columnName.Valid == true){
-            fmt.Printf(" columnName:  %s \n", tableMeta.columnName.String)
-        }else{
-            fmt.Printf(" column name: It:s unvalid string \n")
-        }
+        listTableMeta = append(listTableMeta, tableMeta)
 
-        if (tableMeta.datetimePrecision.Valid == true){
-            fmt.Printf(" The date time is  %s \n", tableMeta.datetimePrecision.Int64)
-        }else{
-            fmt.Printf(" datetimePrecison \n")
+        //if (tableMeta.dataType.Valid == true) {
+        //    fmt.Printf("columnType is :  %s \n", tableMeta.dataType.String)
+        //    switch tableMeta.dataType.String {
+        //        case "varchar" : generateString(10)
+        //    }
+        //}else{
+        //    fmt.Printf(" column name: It:s unvalid string \n")
+        //}
+
+
+
+        //if (tableMeta.columnName.Valid == true){
+        //    fmt.Printf(" columnName:  %s \n", tableMeta.columnName.String)
+        //}else{
+        //    fmt.Printf(" column name: It:s unvalid string \n")
+        //}
+
+        //if (tableMeta.datetimePrecision.Valid == true){
+        //    fmt.Printf(" The date time is  %s \n", tableMeta.datetimePrecision.Int64)
+        //}else{
+        //    fmt.Printf(" datetimePrecison \n")
+        //}
+    }
+
+    var colValue string
+    for _, tableMeta := range listTableMeta {
+        if (tableMeta.dataType.Valid == true) {
+            switch tableMeta.dataType.String {
+                case "varchar" : 
+                    colValue = generateString(int(tableMeta.characterMaximumLength.Int64))
+                    fmt.Printf("               The string value is <%s> \n", colValue)
+                case "int":
+                    colValue = generateInt()
+                    fmt.Printf("               The int value is <%s> \n", colValue)
+                case "char":
+                    colValue = generateChar(int(tableMeta.characterMaximumLength.Int64))
+                    fmt.Printf("               The char value is <%s> \n", colValue)
+                case "decimal":
+                    colValue = generateDecimal(int(tableMeta.numericPrecision.Int64), int(tableMeta.numericScale.Int64))
+                    fmt.Printf("               The decimal value is <%s> \n", colValue)
+                default:
+                    fmt.Printf("columnType is :  %s \n", tableMeta.dataType.String)
+            }
         }
     }
+
 
     if rows.Err() != nil {
         fmt.Printf("The error is ", rows.Err())
     }
+
+    var data []string
+    data = append(data, "\"value 01\"")
+    data = append(data, "value 02")
+    data = append(data, "value 03")
+    data = append(data, "value 04")
+    fmt.Printf("The error is %s ", strings.Join(data, ","))
+}
+
+func generateData(tableMeta TableMeta, numRows int){
+}
+
+// Generate the random string
+func generateString(maxSize int) string {
+    b := make([]rune, rand.Intn(maxSize+1))
+    for i := range b {
+        b[i] = letterRunes[rand.Intn(len(letterRunes))]
+    }
+    return string(b)
+}
+
+// Generate random int
+func generateInt() string {
+    return strconv.Itoa(rand.Intn(2147483647))
+}
+// Generate the random string
+func generateChar(size int) string {
+    b := make([]rune, size)
+    for i := range b {
+        b[i] = letterRunes[rand.Intn(len(letterRunes))]
+    }
+    return string(b)
+}
+
+// Generate random decimal
+func generateDecimal(precision int, scale int) string {
+    power10 := math.Pow10(scale)
+    randInt := rand.Intn(int(math.Pow10(precision - scale))) - 1
+
+    return fmt.Sprintf("%d.%d", randInt,  int(math.Round(rand.Float64()*power10)))
 }
