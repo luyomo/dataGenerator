cba08b8289d7567fdec2f58c9d206bb2ddbfc0cd
luyomo <syunka.tyo@gmail.com>
2021-06-26 14:16:33 +0800

    Repleace the varaibles to struct

--- src/main.go
+++ src/main.go
@@ -6,6 +6,19 @@ import (
     _ "github.com/go-sql-driver/mysql"
 )
 
+type TableMeta struct {
+    columnName              sql.NullString     `json:"COLUMN_NAME"`
+    ordinalPosition         sql.NullInt64      `json:"ORDINAL_POSITION"`
+    dataType                sql.NullString     `json:"DATA_TYPE"`
+    characterMaximumLength  sql.NullInt64      `json:"CHARACTER_MAXIMUM_LENGTH"`
+    numericPrecision        sql.NullInt64      `json:"NUMERIC_PRECISION"`
+    numericScale            sql.NullInt64      `json:"NUMERIC_SCALE"`
+    datetimePrecision       sql.NullInt64      `json:"DATETIME_PRECISION"`
+    columnType              sql.NullString     `json:"COLUMN_TYPE"`
+    columnKey               sql.NullString     `json:"COLUMN_KEY"`
+}
+
+
 // Refer to: https://github.com/go-sql-driver/mysql/wiki/Examples
 //           https://medium.com/aubergine-solutions/how-i-handled-null-possible-values-from-database-rows-in-golang-521fb0ee267
 func main() {
@@ -15,8 +28,7 @@ func main() {
     }
     defer db.Close()
 
-    var columnName, dataType, columnType, columnKey sql.NullString
-    var ordinalPosition, characterMaximumLength, numericPrecision, numericScale, datetimePrecision sql.NullInt64
+    var tableMeta TableMeta
 
     var queryStr string = "select COLUMN_NAME, ORDINAL_POSITION, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH, NUMERIC_PRECISION, NUMERIC_SCALE, DATETIME_PRECISION, COLUMN_TYPE, COLUMN_KEY  from information_schema.columns where table_schema = 'dev_tb6290_4' and table_name = 'SummaryCategoryTable' order by ORDINAL_POSITION"
     rows, err := db.Query(queryStr)
@@ -24,82 +36,23 @@ func main() {
     fmt.Printf("Starting before fetching the data ")
     
     for rows.Next() {
-        if err := rows.Scan(&columnName, &ordinalPosition, &dataType, &characterMaximumLength, &numericPrecision, &numericScale, &datetimePrecision, &columnType, &columnKey); err != nil {
-            fmt.Printf("The square number of 13 is: %d", columnName)
+        if err := rows.Scan(&tableMeta.columnName, &tableMeta.ordinalPosition, &tableMeta.dataType, &tableMeta.characterMaximumLength, &tableMeta.numericPrecision, &tableMeta.numericScale, &tableMeta.datetimePrecision, &tableMeta.columnType, &tableMeta.columnKey); err != nil {
+            fmt.Printf("The square number of 13 is: %d", tableMeta.columnName)
         }
-        if (columnName.Valid == true){
-            fmt.Printf(" The error is %s \n", columnName.String)
+        if (tableMeta.columnName.Valid == true){
+            fmt.Printf(" columnName:  %s \n", tableMeta.columnName.String)
         }else{
-            fmt.Printf(" It:s unvalid string ")
+            fmt.Printf(" column name: It:s unvalid string \n")
         }
 
-        if (datetimePrecision.Valid == true){
-            fmt.Printf(" The error is %s \n", datetimePrecision.Int64)
+        if (tableMeta.datetimePrecision.Valid == true){
+            fmt.Printf(" The date time is  %s \n", tableMeta.datetimePrecision.Int64)
         }else{
-            fmt.Printf(" It:s unvalid string \n")
+            fmt.Printf(" datetimePrecison \n")
         }
     }
 
     if rows.Err() != nil {
         fmt.Printf("The error is ", rows.Err())
     }
-
-
-//    // Prepare statement for reading data
-//    stmtOut, err := db.Prepare("select COLUMN_NAME, ORDINAL_POSITION, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH, NUMERIC_PRECISION, NUMERIC_SCALE, DATETIME_PRECISION, COLUMN_TYPE, COLUMN_KEY  from information_schema.columns where table_schema = 'dev_tb6290_4' and table_name = 'SummaryCategoryTable' order by ORDINAL_POSITION")
-//    if err != nil {
-//        panic(err.Error()) // proper error handling instead of panic in your app
-//    }
-//    defer stmtOut.Close()
-//
-//
-//    var rows
-//    rows, err = stmtOut.Query()
-//
-//    // Query the square-number of 13
-//    for rows.Next() {
-//        err = rows.Scan(&columnName, &ordinalPosition, &dataType, &characterMaximumLength, &numericPrecision, &numericScale, &datetimePrecision, &columnType, &columnKey) 
-//        if err != nil {
-//            panic(err.Error()) // proper error handling instead of panic in your app
-//        }
-//        fmt.Printf("The square number of 13 is: %d", columnName)
-//    }
-
-    // Prepare statement for inserting data
-    //stmtIns, err := db.Prepare("INSERT INTO squareNum VALUES( ?, ? )") // ? = placeholder
-    //if err != nil {
-    //    panic(err.Error()) // proper error handling instead of panic in your app
-    //}
-    //defer stmtIns.Close() // Close the statement when we leave main() / the program terminates
-
-    //// Prepare statement for reading data
-    //stmtOut, err := db.Prepare("SELECT squareNumber FROM squarenum WHERE number = ?")
-    //if err != nil {
-    //    panic(err.Error()) // proper error handling instead of panic in your app
-    //}
-    //defer stmtOut.Close()
-    //
-    //// Insert square numbers for 0-24 in the database
-    //for i := 0; i < 25; i++ {
-    //    _, err = stmtIns.Exec(i, (i * i)) // Insert tuples (i, i^2)
-    //    if err != nil {
-    //        panic(err.Error()) // proper error handling instead of panic in your app
-    //	}
-    //}
-    //
-    //var squareNum int // we "scan" the result in here
-    //
-    //// Query the square-number of 13
-    //err = stmtOut.QueryRow(13).Scan(&squareNum) // WHERE number = 13
-    //if err != nil {
-    //    panic(err.Error()) // proper error handling instead of panic in your app
-    //}
-    //fmt.Printf("The square number of 13 is: %d", squareNum)
-    //
-    //// Query another number.. 1 maybe?
-    //err = stmtOut.QueryRow(1).Scan(&squareNum) // WHERE number = 1
-    //if err != nil {
-    //    panic(err.Error()) // proper error handling instead of panic in your app
-    //}
-    //fmt.Printf("The square number of 1 is: %d", squareNum)
 }
